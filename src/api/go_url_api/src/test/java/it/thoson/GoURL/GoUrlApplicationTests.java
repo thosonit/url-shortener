63217dcc48fc8ce1761428e37de519a07f5665d9
thosonit <thoson.it@gmail.com>
2026-06-15 17:29:20 +0700

    feat(api): init Spring Boot Go URL API project
    chore: restore .env.example to placeholder values

---
+++ src/api/go_url_api/src/test/java/it/thoson/GoURL/GoUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package it.thoson.GoURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
